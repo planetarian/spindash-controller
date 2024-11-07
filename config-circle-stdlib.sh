f34fc4dcbb84176395a82fa3c3bc1b0ed2d4a869
planetarian <piro@live.com>
2024-11-06 23:43:35 -0600

    adding some parameters

--- config-circle-stdlib.sh
+++ config-circle-stdlib.sh
@@ -4,18 +4,20 @@ cd circle-stdlib
 
 ./configure -r 4 \
     -o MULTICORE=1 \
+    -o STDLIB_SUPPORT=3 \
     -o USEFLASHY=3 \
     -o SERIALPORT=/dev/ttyS17 \
     -o FLASHBAUD=3000000 \
     -o USERBAUD=3000000 \
     -o REBOOTMAGIC=tAgHQP3Lw2NZcW8Uru7jnf \
-    -o SDCARD=~/spindash-controller/libs/circle/boot/sd
-    # \
-    #-o STDLIB_SUPPORT=3
+    -o SDCARD=~/spindash-controller/circle-stdlib/libs/circle/boot/sd
+    
 
 cat >> Config.mk<< EOF
 
 # adjust according to environment
+MULTICORE = 1
+STDLIB_SUPPORT = 3
 USEFLASHY = 3
 SERIALPORT = /dev/ttyS17
 FLASHBAUD = 3000000
